c0acf15d4b20425ea38b46add91d34d25b1ed006
Chetan Sanjay Shivade <chetanshivade34@gmail.com>
2023-07-22 15:10:58 +0530

    h

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_bxGG0WgOKDejZvER2fTq2xZZjfUs8zs",
-  authDomain: "restaurantapp-c2ed6.firebaseapp.com",
-  databaseURL: "https://restaurantapp-c2ed6-default-rtdb.firebaseio.com",
-  projectId: "restaurantapp-c2ed6",
-  storageBucket: "restaurantapp-c2ed6.appspot.com",
-  messagingSenderId: "174416156605",
-  appId: "1:174416156605:web:2ec169ea4ef3e7bb25e4d4",
+  apiKey: "AIzaSyBJx-T5MJHFUNPT47Q9IuQGHYEV2yTva5I",
+  authDomain: "restaurantapp-9fac9.firebaseapp.com",
+  databaseURL: "https://restaurantapp-9fac9-default-rtdb.firebaseio.com",
+  projectId: "restaurantapp-9fac9",
+  storageBucket: "restaurantapp-9fac9.appspot.com",
+  messagingSenderId: "558091328175",
+  appId: "1:558091328175:web:2a378e4eed624bea3d925a",
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
